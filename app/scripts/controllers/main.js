4c4e823e25f52845eeda831fc45ed2bbc3838d97
unknown <pbborel@CZS004.cityzensciences.fr>
2014-11-12 11:28:39 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inventoryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the inventoryApp
+ */
+angular.module('inventoryApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
